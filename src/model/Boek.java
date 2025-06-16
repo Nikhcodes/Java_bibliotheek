1256bd3d866d50abfce73a8457be3317eec2982a
Nikhcodes <nikhiellingard3@gmail.com>
2025-06-16 00:32:54 -0300

    Initial commit with models and database setup

---
+++ src/model/Boek.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Boek {
+        private String isbn;
+        private String titel;
+        private String auteur;
+        private String editie;
+        private boolean beschikbaar;
+}
